ff90001e907758ece1a88ea23dadc2e8264fd16d
Christian <christianbrintnall@gmail.com>
2019-01-05 11:51:50 -0800

    Remove top level files, add gitlab-ci

--- helpers.py
+++
@@ -1,8 +0,0 @@
-def response(errors=[], messages=[]):
-    response = {"status":"success"}
-    if errors:
-        response["errors"] = errors
-        response["status"] = "failed"
-    if messages and not errors:
-        response["data"] = messages
-    return response
