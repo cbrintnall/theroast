9c65104b527f27cd4a21e5923c48117dd1872712
Christian <christianbrintnall@gmail.com>
2019-01-17 17:23:15 -0800

    Update docker-compose to hot reload react

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p> Hello. </p>
+          <p> This is a test. </p>
         </header>
       </div>
     );
