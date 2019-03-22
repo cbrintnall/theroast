3921a8a393c6cbad37a322d51799af7d88d700fd
Christian Brintnall <christianbrintnall@gmail.com>
2019-03-21 19:31:25 -0700

    Add production ready docker file, and development docker-compose

--- src/theroast_backend/static/base/base.js
+++ src/theroast_backend/static/base/base.js
@@ -1,3 +1,29 @@
+/*
+    Expects a SEARCH object, as so:
+    {
+        name: "Roasts",
+        url: "/url/to/search/roasts"
+    }
+*/
+Vue.component('roast-search', {
+  props: ["options"],
+  template: `
+  <form class="form-inline my-2 my-lg-0" v-bind:action="options.url" action="GET">
+    <div class="input-group">
+        <div class="input-group-prepend">
+        <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ options.name }}</button>
+            <div class="dropdown-menu">
+            </div>
+        </div>
+        <input type="text" class="form-control" aria-label="Text input with dropdown button">
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button">Search</button>
+        </div>
+    </div>
+  </form>  
+    `
+});
+
 var app = new Vue({
-    el: '#app'
-})
+  el: "#app"
+});
--- src/theroast_backend/static/home/home.js
+++ src/theroast_backend/static/home/home.js
@@ -1,64 +1,24 @@
-var cRange = `
-<div>
-    <label>Color:</label>
-    <label style="float: right;">
-        {{ this.colors[this.index].name }}
-    </label>
+Vue.component('color-range', {
+    // props: ['name'],
+    data() {
+        return {
+            colors: [
+                { color: "#FFD659", amount: 33},
+                { color: "#7F5B5A", amount: 33},
+                { color: "#403736", amount: 33},
+            ],
+        }
+    },
+    template: `
     <div class="progress">
         <div 
             v-for="c in this.colors"
             class="progress-bar" 
             role="progressbar"
-            v-bind:style="{ width: percent + '%', backgroundColor: c.color }"
+            v-bind:style="{ width: amount + '%', backgroundColor: c.color }"
         >
         </div>
+        <h1>Hello.</h1>
     </div>
-    <input
-        :name="name"
-        class="form-control-range"
-        id="formControlRange"
-        type="range"
-        v-model="count"
-        min="1"
-        max="100"
-        step="1"
-        v-on:change="setColorIndex"
-        v-on:input="setColorIndex"
-    >
-</div>
-`
-
-Vue.component('color-range', {
-    props: ['name'],
-    data() {
-        return {
-            colors: [
-                { name: "Light", color: "#FFD659"},
-                { name: "Medium", color: "#7F5B5A"},
-                { name: "Dark", color: "#403736"},
-            ],
-            count: 0,
-            index: 0,
-        }
-    },
-    computed: {
-        percent() {
-            return ((1 / this.colors.length) * 100)
-        }
-    },
-    methods: {
-        setColorIndex() {
-            var amt_names = this.colors.length
-            this.index = Math.floor(this.count / this.percent)
-
-            if (this.index < 0) {
-                this.index = 0;
-            }
-
-            if (this.index > amt_names - 1) {
-                this.index = amt_names - 1
-            }
-        },
-    },
-    template: cRange
+    `
 })
