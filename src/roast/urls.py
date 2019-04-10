ebed4edeb8d48b8ab8cd49d984b0329e943878a6
Christian Brintnall <christianbrintnall@gmail.com>
2019-04-09 18:39:28 -0700

    Switch URLs to routers

--- src/roast/urls.py
+++ src/roast/urls.py
@@ -1,16 +1,18 @@
 from django.views.generic import TemplateView
-from django.urls import path, reverse_lazy
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 
 from roast.views import *
 
-roasts = Roasts.as_view({
-    'get': 'list',
-    'post': 'create',
-    'put': 'update',
-    'delete': 'destroy'
-})
+roast_router = DefaultRouter()
+roast_router.register(r'', Roasts)
+
+# urlpatterns = [
+#     # path('id/<int:pk>', TemplateView.as_view(template_name='roast.html'),
+#     path('id/<int:pk>', roast_detail, name='roast_detail'),
+#     path('', roasts, name="roasts"),
+# ]
 
 urlpatterns = [
-    path('id/<int:pk>', TemplateView.as_view(template_name='roast.html')),
-    path('', roasts, name="roasts"),
+    path('', include(roast_router.urls))
 ]
